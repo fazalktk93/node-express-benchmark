4ec0875e89977801af2216db249c2cc86a49f7e3
Fazal ur Rehman <fazalktk93@gmail.com>
2025-06-16 23:37:25 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`Listening: http://0.0.0.0:${port}`);
 });
